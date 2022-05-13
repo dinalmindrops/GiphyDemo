e8112732347a69d8a7baf488f294765209ef6425
KoodosDev <koodosdev@gmail.com>
2022-05-13 16:57:55 +0530

    Initial Commit

---
+++ GiphyDemo/GiphyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiphyDemo
+//
+//  Created by Dinal's iMac on 13/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
