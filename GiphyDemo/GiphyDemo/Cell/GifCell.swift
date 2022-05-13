072dd435f717d63aa90d947daf76d39e33bc72e7
KoodosDev <koodosdev@gmail.com>
2022-05-13 19:05:57 +0530

    UI done

---
+++ GiphyDemo/GiphyDemo/Cell/GifCell.swift
@@ -0,0 +1,12 @@
+//
+//  GifCell.swift
+//  GiphyDemo
+//
+//  Created by Dinal's iMac on 13/05/22.
+//
+
+import UIKit
+
+class GifCell: UICollectionViewCell {
+    
+}
--- GiphyDemo/GiphyDemo/ViewController.swift
+++ GiphyDemo/GiphyDemo/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+    
+    @IBOutlet weak var txtSearchBar: UISearchBar!
+    @IBOutlet weak var clvGifs: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        clvGifs.delegate = self
+        clvGifs.dataSource = self
+        
+        txtSearchBar.delegate = self
     }
 
-
+    //MARK:- Search bar view methods
+   
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        txtSearchBar.endEditing(true)
+    }
+    
+    //MARK:- Collection view methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = clvGifs.dequeueReusableCell(withReuseIdentifier: "GifCell", for: indexPath) as! GifCell
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width/2, height: collectionView.frame.size.width/2)
+    }
 }
 
